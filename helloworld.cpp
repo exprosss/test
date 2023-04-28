533b68d90a9a421b5178f85d9fa2aa4ee61526af
exprosss <shestov1804@gmail.com>
2023-04-28 14:57:51 +0000

    second task

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+int fib(int n) {
+  if (n==0) {
+    return 0;
+  } else if (n==1) {
+    return 1;
+  } else {
+    return fib(n-1)+fib(n-2);
+  }
+}
 using namespace std;
 int main() {
-    cout<<"Hello world";
+    cout<<"Hello world"<<endl;
+    int n; cout<<"Введите n: "; cin>>n;
+    cout<<fib(n);
 }
